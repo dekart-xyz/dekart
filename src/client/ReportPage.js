ba206b77e680423978ea991ede5cd8440e974938
Vladi Bilonenko <bilonenko.v@gmail.com>
2020-12-16 07:13:25 +0100

    Add SQL Editor

--- src/client/ReportPage.js
+++ src/client/ReportPage.js
@@ -1,8 +1,28 @@
 import { useParams } from 'react-router-dom'
+import AceEditor from 'react-ace'
+
+import 'ace-builds/src-noconflict/mode-sql'
+import 'ace-builds/src-noconflict/theme-textmate'
+import 'ace-builds/src-noconflict/ext-language_tools'
+import 'ace-builds/webpack-resolver'
 
 export default function ReportPage () {
   const { id } = useParams()
   return (
-    <div>Report {id}</div>
+    <div>
+      <div>Report {id}</div>
+      <AceEditor
+        mode='sql'
+        theme='textmate'
+        // onChange={onChange}
+        name='UNIQUE_ID_OF_DIV'
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: true
+        }}
+      />
+    </div>
   )
 }
