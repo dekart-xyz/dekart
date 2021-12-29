71b2fa3a062e0148d3545f1fe41aea72142ad3ec
Vladi Bilonenko <bilonenko.v@gmail.com>
2021-12-29 08:56:42 +0100

    Fix vulnerabilities

--- src/client/DekartMenu.js
+++ src/client/DekartMenu.js
@@ -12,17 +12,17 @@ export default function DekartMenu () {
   return (
     <div className={styles.dekartMenu}>
       <Menu mode='horizontal' theme='dark'>
-        <Menu.Item>
+        <Menu.Item key='reports'>
           <a href='/'>Reports</a>
         </Menu.Item>
-        <Menu.SubMenu popupClassName={styles.subMenu} title='Community' active>
-          <Menu.Item>
+        <Menu.SubMenu popupClassName={styles.subMenu} title='Community' key='community' active='yes'>
+          <Menu.Item key='documentation'>
             <a target='_blank' rel='noopener noreferrer' href={'https://dekart.xyz/docs/?ref=' + ref}>Documentation</a>
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item key='github'>
             <a target='_blank' rel='noopener noreferrer' href={'http://github.com/dekart-xyz/dekart/?ref=' + ref}>GitHub</a>
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item key='issues'>
             <a target='_blank' rel='noopener noreferrer' href={'https://github.com/dekart-xyz/dekart/issues?ref=' + ref}>Report Issue</a>
           </Menu.Item>
         </Menu.SubMenu>
