42b0b42cc8ba1500c21815d259ff4451537942c3
Vladi Bilonenko <bilonenko.v@gmail.com>
2022-10-28 07:58:23 +0200

    Style file uplaod  first step

--- src/client/File.js
+++ src/client/File.js
@@ -1,4 +1,23 @@
+import Upload from 'antd/lib/upload/Upload'
+import styles from './File.module.css'
+import { InboxOutlined, UploadOutlined } from '@ant-design/icons'
+import Button from 'antd/es/button'
 
 export default function File () {
-  return null
+  return (
+    <div className={styles.file}>
+      <div className={styles.info}>
+        <div className={styles.upload}>
+          <Upload onDrop={() => {}}>
+            <div className={styles.uploadIcon}><InboxOutlined /></div>
+            <div className={styles.uploadHeader}>Click or drag file to this area to upload</div>
+            <div className={styles.uploadSubtitle}>Supported format: .csv, .geojson</div>
+          </Upload>
+        </div>
+      </div>
+      <div className={styles.status}>
+        <Button icon={<UploadOutlined />} disabled>Upload</Button>
+      </div>
+    </div>
+  )
 }
