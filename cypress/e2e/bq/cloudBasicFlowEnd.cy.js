f1b1006f0e4c587f340b37eed12ab39d1b022f08
Vladi Bilonenko <bilonenko.v@gmail.com>
2024-03-10 21:14:22 +0100

    Add e2e test

--- cypress/e2e/bq/cloudBasicFlowEnd.cy.js
+++ cypress/e2e/bq/cloudBasicFlowEnd.cy.js
@@ -3,14 +3,8 @@ import copy from '../../fixtures/copy.json'
 
 describe('cloud basic flow', () => {
   it('should make simple bigquery query and get ready status', () => {
-    // create workspace
-    cy.visit('/')
-    cy.get('button:contains("Create workspace")').click()
-    cy.get('input#name').type('test')
-    cy.get('button:contains("Create")').click()
-    cy.get('button#dekart-1-choose-plan').click()
-
     // create connection
+    cy.visit('/')
     cy.get('button:contains("Create connection")').click()
     const randomConnectionName = `test-${Math.floor(Math.random() * 1000000)}`
     cy.get('div.ant-modal-title').should('contain', 'Edit Connection')
