30b95ef9dc4967175efc0f51a20df6ae82404c71
Vladi Bilonenko <bilonenko.v@gmail.com>
2024-05-26 11:39:58 +0200

    Update DEKART_BIGQUERY_MAX_BYTES_BILLED to increase the limit

--- pulumi/main.go
+++ pulumi/main.go
@@ -98,7 +98,7 @@ func main() {
 				"DEKART_UX_HOMEPAGE":               pulumi.String("/"),
 				"DEKART_BIGQUERY_PROJECT_ID":       pulumi.String("dekart-playground"),
 				"DEKART_CLOUD_STORAGE_BUCKET":      pulumi.String("dekart-playground"),
-				"DEKART_BIGQUERY_MAX_BYTES_BILLED": pulumi.String("4294967296"),
+				"DEKART_BIGQUERY_MAX_BYTES_BILLED": pulumi.String("107374182400"),
 				"DEKART_UX_SAMPLE_QUERY_SQL":       pulumi.String("SELECT primary_type, district, latitude, longitude, date from `bigquery-public-data.chicago_crime.crime` WHERE  Rand() < 5 / 100.0"),
 				"DEKART_CORS_ORIGIN":               pulumi.String("null"),
 			},
