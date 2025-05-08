0b7c472507e9948796372d4f78f749507f00cbd0
Vladi Bilonenko <bilonenko.v@gmail.com>
2025-05-08 07:44:48 +0200

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Add this line:
 const defineProcessEnv = {
   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
 }
