8cdaa5a9d12531d95d9a6e1ad0a9e5632ddb1945
Vladi Bilonenko <bilonenko.v@gmail.com>
2026-09-07 09:39:20 +0200

    feat: enhance device authorization logic and improve query execution handling

--- src/server/user/claims_test.go
+++ src/server/user/claims_test.go
@@ -5,14 +5,12 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"crypto/sha256"
-	"encoding/base64"
-	"encoding/json"
 	"net/http"
 	"sync"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/golang-jwt/jwt"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,46 +28,14 @@ func TestValidateJWTFromAmazonOIDC(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Create a header
-	header := map[string]string{
-		"alg": "ES256",
-		"kid": "testKid",
-	}
-	headerBytes, err := json.Marshal(header)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create a payload with an email claim
-	payload := map[string]string{
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
 		"email": "test@example.com",
-	}
-	payloadBytes, err := json.Marshal(payload)
+	})
+	token.Header["kid"] = "testKid"
+	tokenString, err := token.SignedString(privateKey)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// Base64 encode the header and payload using old JWT spec, new spec uses base64.RawURLEncoding.EncodeToString
-	encodedHeader := base64.StdEncoding.EncodeToString(headerBytes)
-	encodedPayload := base64.StdEncoding.EncodeToString(payloadBytes)
-
-	// Concatenate the encoded header and payload with a period separator
-	message := encodedHeader + "." + encodedPayload
-
-	// Sign the message with the private key
-	hash := sha256.Sum256([]byte(message))
-	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hash[:])
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Base64 encode the signature
-	signature := r.Bytes()
-	signature = append(signature, s.Bytes()...)
-	encodedSignature := base64.RawURLEncoding.EncodeToString(signature)
-
-	// Concatenate the signature with the message with a period separator
-	tokenString := message + "." + encodedSignature
 
 	claimsCheck := ClaimsCheck{
 		ClaimsCheckConfig: ClaimsCheckConfig{
@@ -83,7 +49,7 @@ func TestValidateJWTFromAmazonOIDC(t *testing.T) {
 	}
 
 	// Store the public key
-	claimsCheck.publicKeys.Store(header["kid"], &privateKey.PublicKey)
+	claimsCheck.publicKeys.Store("testKid", &privateKey.PublicKey)
 
 	// Mock the http request
 	req, err := http.NewRequest("GET", "/", nil)
